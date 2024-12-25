67e2b53eb949efe43b142097ed795954511da825
AdammY1 <admye@ucdavis.edu>
2024-12-25 13:22:13 -0500

    vector version

---
+++ 3203_tree/Trial_vector.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    struct Node{
+        vector<int> links;
+    };
+
+    void readInTree(const vector<vector<int>> edges, vector<Node>* Tree){
+        for(auto element : edges){
+            int a = element[0];
+            int b = element[1];
+            (*Tree)[a].links.push_back(b);
+            (*Tree)[b].links.push_back(a);
+        }
+    }
+
+    int shortestMaxLen( const vector<Node>& Tree , int *maxDiameter){
+        if(Tree.empty()) return 0;
+        int min = INT_MAX;
+        int temp;
+        for(int i=0; i<Tree.size(); i++){ 
+            temp = maxLen(Tree, i, Tree[i], -1);
+            min = (temp < min) ? temp : min ;
+            *maxDiameter = (temp > (*maxDiameter)) ? temp : *maxDiameter ;
+        }
+        return min;
+    }
+
+    // BFS in essence
+    int maxLen(const vector<Node>& Tree,int rootIdx, Node root, int parent){
+        int max = -1;
+        int temp;
+        for(auto idx : root.links){
+            if(idx == parent) continue;
+            temp = maxLen(Tree, idx, Tree[idx], rootIdx);
+            max = (temp > max) ? temp : max;
+        }
+        return 1 + max;  // if empty, max = -1 and it happen to add up to 0 with 1 + max
+    }
+
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+        vector<Node> Tree1(edges1.size() + 1);
+        vector<Node> Tree2(edges2.size() + 1);
+        
+        readInTree(edges1,&Tree1);
+        readInTree(edges2,&Tree2);
+
+        int tree1Max = 0;
+        int tree2Max = 0;
+
+        return max(shortestMaxLen(Tree1, &tree1Max) + 1 + shortestMaxLen(Tree2, &tree2Max), max(tree1Max, tree2Max));
+    }
+};
+
+int main(){
+    
+    vector<vector<int>> edges1 = {{0,1},{0,2},{0,3},{2,4},{2,5},{3,6},{2,7}};
+    vector<vector<int>> edges2 = {{0,1},{0,2},{0,3},{2,4},{2,5},{3,6},{2,7}};
+    
+    // vector<vector<int>> edges1 = {};
+    // vector<vector<int>> edges2 = {};
+    
+    // tricky case, original diameter is longer...
+    // vector<vector<int>> edges1 = {{0,1},{2,0},{3,2},{3,6},{8,7},{4,8},{5,4},{3,5},{3,9}};
+    // vector<vector<int>> edges2 = {{0,1},{0,2},{0,3}};
+
+    Solution sol;
+    cout<< sol.minimumDiameterAfterMerge(edges1,edges2) << endl;
+
+    return 0;
+}
