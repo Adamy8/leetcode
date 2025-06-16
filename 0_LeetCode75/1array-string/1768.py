631a7082b2d79c60e9290b9be5674f152effcb8a
Adamy <admye@ucdavis.edu>
2025-06-16 00:05:30 -0700

    1768 easy

--- 0_LeetCode75/1array-string/1768.py
+++ 0_LeetCode75/1array-string/1768.py
@@ -1,3 +1,20 @@
+# easy
+# 1768. Merge Strings Alternately
+
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        result=''
+        i = 0
+        while i<len(word1) and i<len(word2):
+            result += (word1[i])
+            result += (word2[i])
+            i+=1
+        j = i
+        while i<len(word1) :
+            result += (word1[i])
+            i+=1
+        while(j<len(word2)):
+            result += (word2[j])
+            j+=1
+        return result
         
