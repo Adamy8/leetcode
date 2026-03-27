9af52618ecb010cb6023c00fda0117060bf92978
Adamy <admye@ucdavis.edu>
2026-03-27 01:15:28 -0400

    twosumII - two pointer

---
+++ 167_TwoSum2-twopointer/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int a=0, b=numbers.size()-1;
+        while(a<b){
+            if(numbers[a]+numbers[b]==target) return{a+1,b+1};
+            else if(numbers[a]+numbers[b]<target) a++;
+            else b--;
+        }
+        return {};
+    }
+};
