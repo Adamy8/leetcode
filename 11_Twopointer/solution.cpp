c687fac06961ad8cd7ca90a6200b4fb63c490caa
Adamy <admye@ucdavis.edu>
2026-03-27 15:54:21 -0400

    11 most water

---
+++ 11_Twopointer/solution.cpp
@@ -0,0 +1,19 @@
+#include<cmath>
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int L=0, R=height.size()-1;
+        int width = R-L;
+        int maxA = min(height[L],height[R]) * width;
+        while(R>L){
+            if(height[L]<height[R]){
+                L++;
+            }else R--;
+            width--;
+            maxA = max(min(height[L],height[R]) * width, maxA);
+        }
+        return maxA;
+    }
+}; 
+
+// 11. most water container
