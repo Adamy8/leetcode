6425e6eed0709ace6559655566c62f0df9f2dd99
Adamy <admye@ucdavis.edu>
2026-04-09 02:47:30 -0700

    optimised w dp, finally passed

---
+++ 329_DFSBFSDPpq/hard.cpp
@@ -0,0 +1,50 @@
+// use struct or tuple
+#include<queue>
+#include<algorithm>
+struct Node{
+    int val;
+    int x,y;
+};
+class Solution {
+public:
+    int maxDepth(const Node& root,vector<vector<int>>& matrix, vector<vector<int>>& dp){
+        if (dp[root.x][root.y] != 0) return dp[root.x][root.y];     // improve BigO w dp
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int val = matrix[root.x][root.y];
+
+        int l = 1, r = 1, u = 1, d = 1;
+
+        if (root.x > 0 && val < matrix[root.x - 1][root.y])
+            u = 1 + maxDepth({matrix[root.x - 1][root.y], root.x-1, root.y}, matrix, dp);
+        if (root.y > 0 && val < matrix[root.x][root.y - 1])
+            l = 1 + maxDepth({matrix[root.x][root.y - 1], root.x, root.y-1}, matrix, dp);
+        if (root.x < m - 1 && val < matrix[root.x + 1][root.y])
+            d = 1 + maxDepth({matrix[root.x + 1][root.y], root.x+1, root.y}, matrix, dp);
+        if (root.y < n - 1 && val < matrix[root.x][root.y + 1])
+            r = 1 + maxDepth({matrix[root.x][root.y+1], root.x, root.y + 1}, matrix, dp);
+
+        int ans = max(max(l,r),max(u,d));
+        dp[root.x][root.y] = ans;
+        return ans;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m = matrix.size(); //row
+        int n = matrix[0].size(); //column
+        queue<Node> q;
+        for(int i=0;i<m; i++){
+            for(int j=0; j<n; j++){
+                q.push({matrix[i][j],i,j});
+            }
+        }
+        int max = 0, temp;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        while(q.size()!=0){
+            temp = maxDepth(q.front(), matrix, dp);
+            q.pop();
+            if (temp > max) max = temp;
+        }
+        return max;
+    }
+};
