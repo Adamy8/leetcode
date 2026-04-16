6af0c7e2b2c27b6de1a17b3df66228c1be11845d
Adamy <admye@ucdavis.edu>
2026-04-16 01:55:23 -0700

    so many boundary trap

--- 228_twopointer/easy.cpp
+++ 228_twopointer/easy.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
+        if(! nums.size()) return ans;
+
         int start = 0,end = 0, curr = nums[0];
         for(int i=1;i<nums.size();i++){
-            if(nums[i]-curr == 1){
+            if(nums[i] - 1 == curr){
+                curr = nums[i];
                 end++;
-                continue;
             }
             else{
                 if (start != end)
@@ -18,6 +20,10 @@ public:
                 curr = nums[start];
             }
         }
+        if (start != end)
+            ans.push_back(to_string(nums[start]) + "->" + to_string(nums[end]));
+        else
+            ans.push_back(to_string(nums[start]));
         return ans;
     }
 };
