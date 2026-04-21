a2da54b1fb7c04f260781c1943f3111d96855d3b
Adamy <admye@ucdavis.edu>
2026-04-21 02:28:57 -0700

    spiral matrix simulation

---
+++ 54_array/medium.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();  // row, 3 in ex2
+        vector<int> ans;
+        if(!m) return ans;
+        int n = matrix[0].size();   // col, 4 in ex2
+
+        int left = 0, top = 0;  //they store the available boundary
+        int right = matrix[0].size()-1, down = matrix.size()-1;
+
+        for(int i=0; i<m*n; ){
+            // add in order or top-right-down-left
+            if(right>=left){ // top
+                for(int j=left; j<=right; j++){
+                    ans.push_back(matrix[top][j]);
+                    i++;
+                }
+                top++;
+            }
+            if(down>=top){ //right
+                for(int j=top; j<=down; j++){
+                    ans.push_back(matrix[j][right]);
+                    i++;
+                }
+                right--;
+            }
+            if(left<=right){ //down
+                for(int j=right; j>=left; j--){
+                    ans.push_back(matrix[down][j]);
+                    i++;
+                }
+                down--;
+            } 
+            if(top<=down){  //left
+                for(int j=down; j>=top; j--){
+                    ans.push_back(matrix[j][left]);
+                    i++;
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
