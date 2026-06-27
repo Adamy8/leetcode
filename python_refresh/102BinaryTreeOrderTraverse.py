5d00be5ad1fe0a85c36835af769df32d48d73e86
Adamy <admye@ucdavis.edu>
2026-06-27 22:58:06 +0800

    BFSOn Ow DFSOn Oh

---
+++ python_refresh/102BinaryTreeOrderTraverse.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root: return []
+        dq = deque([root])
+
+        ans = []
+        while dq:
+            temp = []
+            for _ in range(len(dq)):
+                node = dq.popleft()
+                temp.append(node.val)
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+            ans.append(temp)
+        return ans
+# my solution, BFS with deque, O(n) time, O(n) space. 
+# queue alone is O(w) space. But we also need to store the answer, so O(n) space in total.
+# O(w) in worse case w = n/2 = O(n) in a complete binary tree.
+
+
+# there's also a recrusive solution, DFS with recursion, O(n) time, O(h) space. h is the height of the tree. In worse case h = n in a skewed tree.
