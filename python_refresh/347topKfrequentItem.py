03775c9b0f76e71c42a5467386e850b4e83a4616
Adamy <admye@ucdavis.edu>
2026-06-28 13:41:51 +0800

    counter method, hella short code

--- python_refresh/347topKfrequentItem.py
+++ python_refresh/347topKfrequentItem.py
@@ -22,3 +22,14 @@ class Solution:
 # might have same frequency, so we need to store a list of values at each frequency index. 
 # Then we can iterate from the end of the vector to get the k largest frequency values. 
 # This approach is O(n) time and O(n) space.
+
+
+# another way is to use from collections import Counter, then use most_common(k) to get the k most common elements. This is also O(n) time and O(n) space.
+# really goddamn easy to implement
+from collections import Counter
+
+cnt = Counter([1,1,2,2,2,5])
+
+print(cnt[1])  # 2
+print(cnt[2])  # 3
+print(cnt[5])  # 1
